e38530a9df98205d64073b2e97eaeaa1d3a2c118
akshat tiwari <akshatmtiwari@gmail.com>
2025-07-08 23:06:26 +0530

    handle key presses

--- src/reactive-ui/Modal/index.js
+++ src/reactive-ui/Modal/index.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Portal from '../Portal'
 import './index.css'
 
-function Modal({children, title,titleStyle,modalCloseStyle,modalContentStyle,modalHeaderStyle,modalOverlayStyle,isOpen,actions,actionStyles}) {
+function Modal({children, title,titleStyle,modalCloseStyle,modalContentStyle,modalHeaderStyle,modalOverlayStyle,isOpen,actions,actionStyles,onClose,submitButtonOnEnter='Submit'}) {
+
+    useEffect(()=>{
+
+        const handleKeyDown = (event) => {
+            if (event.key === 'Escape') {
+                onClose();
+            }
+
+            if(event.key === 'Enter' && submitButtonOnEnter){
+                const submitButton = document.getElementById(`modal-action-${submitButtonOnEnter}`);
+                if(submitButton){
+                    submitButton.click();
+                }
+            }
+        }
+
+        if(isOpen){
+            window.addEventListener('keydown',handleKeyDown);
+        }
+
+        return () => {
+            window.removeEventListener('keydown',handleKeyDown);
+        }
+
+    },[isOpen]);
 
     if(!isOpen) return null;
 
@@ -12,7 +37,7 @@ function Modal({children, title,titleStyle,modalCloseStyle,modalContentStyle,mod
                 <div style={{...modalContentStyle}} className="modal-content">
                     <div style={{...modalHeaderStyle}} className="modal-header">
                         {title && <h2 style={{titleStyle}} >{title}</h2>}
-                        <button style={{...modalCloseStyle}} className="modal-close"> &times; </button>
+                        <button onClick={onClose} style={{...modalCloseStyle}} className="modal-close"> &times; </button>
                     </div>
                     <div className='modal-children' >
                         {children}
@@ -20,7 +45,7 @@ function Modal({children, title,titleStyle,modalCloseStyle,modalContentStyle,mod
                     <div style={{...actionStyles}} className='modal-actions'>
                         {
                             actions.map((action, index) => (
-                                <button key={index} onClick={action.onClick} style={{...action.style}} className="modal-action-button">
+                                <button key={index} onClick={action.onClick} style={{...action.style}} id={`modal-action-${action.label}`} className="modal-action-button">
                                     {action.label}
                                 </button>
                             ))
