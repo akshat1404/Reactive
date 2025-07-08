408ef17610786ae23db27dec8ee1cfa6d89f495e
akshat tiwari <akshatmtiwari@gmail.com>
2025-07-08 22:24:08 +0530

    modal done

--- src/reactive-ui/Modal/index.js
+++ src/reactive-ui/Modal/index.js
@@ -2,20 +2,34 @@ import React from 'react'
 import Portal from '../Portal'
 import './index.css'
 
-function Modal({children}) {
-  return (
-    <Portal>
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h2>Modal Title</h2>
-                    <button className="modal-close"> &times; </button>
+function Modal({children, title,titleStyle,modalCloseStyle,modalContentStyle,modalHeaderStyle,modalOverlayStyle,isOpen,actions,actionStyles}) {
+
+    if(!isOpen) return null;
+
+    return (
+        <Portal>
+            <div style={{...modalOverlayStyle}} className="modal-overlay">
+                <div style={{...modalContentStyle}} className="modal-content">
+                    <div style={{...modalHeaderStyle}} className="modal-header">
+                        {title && <h2 style={{titleStyle}} >{title}</h2>}
+                        <button style={{...modalCloseStyle}} className="modal-close"> &times; </button>
+                    </div>
+                    <div className='modal-children' >
+                        {children}
+                    </div>
+                    <div style={{...actionStyles}} className='modal-actions'>
+                        {
+                            actions.map((action, index) => (
+                                <button key={index} onClick={action.onClick} style={{...action.style}} className="modal-action-button">
+                                    {action.label}
+                                </button>
+                            ))
+                        }   
+                    </div>
                 </div>
-                {children}
             </div>
-        </div>
-    </Portal>
-  )
+        </Portal>
+    )
 }
 
-export default Modal
+export default Modal;
