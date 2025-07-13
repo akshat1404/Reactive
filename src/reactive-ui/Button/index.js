bc9dc15abac44f95ca1875f946fcc0ef0fda34e7
akshat tiwari <akshatmtiwari@gmail.com>
2025-07-13 20:18:52 +0530

    changes

--- src/reactive-ui/Button/index.js
+++ src/reactive-ui/Button/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './index.css';
+import Loader from '../Loader';
+import { debounce } from '../Utils';
 
-function Button({className='primary', title, onClick, style, disabled=false,loading=false,loaderType}) {
-    
+function Button({className='primary', title, onClick, style, disabled=false, loading=false, loaderType="wave", debounce=false,debounceDelay=300}) {
+
+    let CLICK=useRef(onClick);
+
+    useEffect(()=>{
+
+        if(debounce){
+            CLICK.current = debounce(onClick, debounceDelay);
+        }
+    },[]);
+
     return (
         <>
-            <button style={{...style}} className={`btn ${className}`} onClick={onClick} >
-                {title}
+            <button disabled={disabled || loading} style={{...style}} className={`btn ${className}`} onClick={CLICK.current} >
+                {loading ? <Loader color="white" type={loaderType} /> : title}
             </button>
         </>
     );
